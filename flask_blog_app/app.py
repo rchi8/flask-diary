879526fc1e941c724bfb4bf6e7b1ab70ff4a0505
ryohei8 <ryohei.chihara.web@gmail.com>
2023-09-27 22:34:05 +0900

    add アプリの基本構造追加

---
+++ flask_blog_app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+posts = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', posts=posts)
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/store', methods=['POST'])
+def store():
+    title = request.form['title']
+    content = request.form['content']
+    posts.append({'title': title, 'content': content})
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
